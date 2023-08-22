7e7ef5883b2a1c32ab7964b7c810b49a49c00764
keunoh <rmsdhcjswo1@gmail.com>
2023-08-22 23:51:35 +0900

    sqlp

---
+++ Script.sql
@@ -0,0 +1,181 @@
+SELECT * FROM EMP;
+SELECT * FROM DEPT;
+
+-- 문제점 기술 후 튜닝
+/*
+ * 1. 입금일시 컬럼을 변환했으므로 인덱스 RANGE SCAN 불가
+ * 2. 수납일자 조건 자동 형변환 발생하므로 RANGE SCAN 불가
+ * 3. LITERAL SQL 의한 하드파싱 부하
+ * 4. 절차적 루프 처리에 의한 반복 DB CALL
+ * 5. 루프 내에서 건건 커밋
+ * */
+DECLARE
+	L_수납금액 NUMBER;
+BEGIN
+	FOR C IN (SELECT 고객ID, SUM(입금액) 입금액
+				FROM 은행입금내역
+			   WHERE TO_CHAR(입금일시, 'YYYYMMDD') = '20210329' -- 1
+			   GROUP BY 고객ID)
+	LOOP	
+		BEGIN
+			SELECT 수납금액 INTO L_수납금액
+			  FROM 수납
+			 WHERE 고객ID = C.고객ID
+			   AND 수납일자 = 20210329; -- 2
+			  EXECUTE IMMEDIATE
+			  	' UPDATE 수납 SET 수납금액 = ' || C.입금액 ||
+			  	'  WHERE 고객 ID = ' || C.고객ID ||
+			  	'    AND 수납일자 = 20210329';
+		EXCEPTION
+			WHEN NO_DATA_FOUND THEN
+				'INSERT INTO 수납(고객ID, 수납일자, 수납금액) VALUES (' ||
+				C.고객ID || ', 20210329, ' || C.입금액 || ')';
+		END;
+	COMMIT;
+	END LOOP;
+END;
+
+-- [튜닝 SQL]
+MERGE INTO 수납 R
+USING (SELECT 고객ID, SUM(입금액) 입금액
+		 FROM 은행입금애녁
+		WHERE 입금일시 BETWEEN TO_DATE('20210329', 'YYYYMMDD') AND TO_DATE('20210330', 'YYYYMMDD') - 1/24/60/60
+		GROUP BY 고객ID) I
+   ON (R.고객ID = I.고객ID AND R.수납일자 = '20210329')
+ WHEN MATCHED THEN UPDATE
+ 	SET R.수납금액 = I.입금액
+ WHEN NOT MATCHED THEN
+ 	INSERT (고객ID, 수납일자, 수납금액) VALUES (I.고객ID, '20210329', I.입금액);
+ COMMIT;
+ 
+CREATE INDEX 회원_X01 ON 회원(생년월일);
+CREATE INDEX 회원_X02 ON 회원(생년월일, 전화번호);
+CREATE INDEX 회원_X03 ON 회원(생년월일, 회원명, 전화번호);
+CREATE INDEX 회원_X04 ON 회원(전화번호);
+
+SELECT *
+  FROM 회원
+ WHERE 생년월일 LIKE '1979%'
+   AND 전화번호 = ENCRYPTION(:PHONE);
+   
+-- 튜닝 후
+SELECT DISTINCT GET_USERNAME(사원id)
+  FROM 휴가기록
+ WHERE 휴가일자 >= ADD_MONTHS(SYSDATE, -3)
+ GROUP BY 사원ID; 
+ 
+-- 주문 테이블 파티셔닝
+/* 1. 주문 테이블에 입력하려고 백업해 둔 임시 테이블에는 2020년 1월부터 2021년 10월까지이 주문데이터가 입력되어있음
+ * 2. 예상치 못한 주문일시가 입력되더라도 에러가 발생하지 않아야함
+ * 3. 파티션 명명규칙은 자유 
+ */
+CREATE TABLE 주문 (
+	주문번호 NUMBER
+  , 주문일시 DATE
+  , 고객ID  VARCHAR2(5)
+  , 주문금액 NUMBER
+)
+PARTITION BY RANGE(주문일시)
+( PARTITION P2020_H1 VALUES LESS THAN (TO_DATE('20200701', 'YYYYMMDD'))
+, PARTITION P2020_H2 VALUES LESS THAN (TO_DATE('20210101', 'YYYYMMDD') 
+, PARTITION P2021_H1 VALUES LESS THAN (TO_DATE('20210701', 'YYYYMMDD') 
+, PARTITION P2021_H2 VALUES LESS THAN (TO_DATE('20220101', 'YYYYMMDD') 
+, PARTITION P9999_MX VALUES LESS THAN (MAXVALUE));
+
+-- 튜닝 전 -> 후
+SELECT  A.*
+	  , (SELECT NVL(MAX('Y'), 'N') 
+	  	   FROM 자료전송이력 
+	  	  WHERE 상담원ID = A.상담원ID 
+	  	    AND 상담일시 = A.상담일시 
+	-- 추가  AND 등록일시 >= 상담일시  
+	  	    AND ROWNUM = 1) 자료전송여부
+  FROM (
+  		SELECT 상담원ID, 상담일시, 상담접촉구분코드
+  			 , 연락전화번호, 통화자명, 호식별번호
+  			 , 상담처리상태코드, 조직ID
+  		  FROM 고객상담
+  		 WHERE 고객번호 = :CUST_NUM
+  		 ORDER BY 고객번호 DESC, 상담일시 DESC) A
+  	) 
+ WHERE ROWNUM <= 10; 
+/*
+ * SORT AGGREGATE
+ * 	 COUNT STOPKEY
+ * 	   PARTITION RANGE ALL -> PARTITION RANGE ITERATOR
+ * 	     INDEX RANGE SCAN
+ * COUNT STOPKEY
+ *   VIEW
+ *     TABLE ACCESS BY INDEX ROWID
+ *       INDEX RANGE SCAN DESCENDING 
+ */
+
+-- 튜닝 전
+-- [테이블 정보]
+/* 고객 : 1,000만명
+ * 고객변경이력 : 변경일자 기준 월단위 RANGE 파티션
+ * */
+-- [인덱스 구성]
+/* 고객_PK : [고객번호]
+ * 고객_N1 : [고객번호 + 고객상태코드 + 상태변경일자]
+ * 고객변경이력_PK : [고객번호 + 변경일자 + 변경구분코드 + 변경일련번호]
+ * */
+SELECT *
+  FROM (
+  		SELECT 고객번호, MAX(변경일자) 변경일자
+  			 , MAX(변경구분코드) KEEP (DENSE_RANK LAST ORDER BY 변경일자, 변경일련번호) 변경구분코드
+  		  FROM 고객변경이력 CH
+  		 WHERE EXISTS (SELECT 'X' FROM 고객 C WHERE 고객번호 = CH.고객번호 AND 상태변경일자 <= CH.변경일자 AND 고객상태코드 = 'AI')
+   -- 추가  AND 변경일자 >= :CHG_DT
+  		 GROUP BY 고객번호
+  	)
+ WHERE 변경일자 = :CHG_DT
+   AND 변경구분코드 = 'D1';
+/* FILTER
+ *   SORT (GROUP BY)
+ * 	   HASH JOIN (SEMI)
+ * 	     INDEX (RANGE SCAN) OF '고객_N1' (INDEX)
+ *  	 PARTITION RANGE (ALL) -> PARTITION RANGE (ITERATOR)
+ * 		   INDEX (FAST FULL SCAN) OF '고객변경이력_PK' (INDEX)
+ * */
+  
+ALTER SESSION SET SQL_TRACE = TRUE;
+
+SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY_CURSOR('5rsm4y10jd4p2',0,'ALLSTATS'));
+EXPLAIN PLAN FOR
+SELECT * FROM EMP;
+
+-- 튜닝 전 -> 후
+SELECT 주문번호, 주문일시
+  FROM 주문
+ WHERE 주문상태코드 IN ('0', '1', '2', '4', '5')
+   AND 주문일자 BETWEEN :DT1 AND :DT2;
+
+-- 튜닝 전 -> 후
+/* [인덱스 구성]
+ * 월말게좌상태_PK : [계좌번호 + 계좌일련번호 + 기준년월 ]
+ * 월말계좌상태_X1 : [기준년월 + 상태구분코드 ]
+ */
+UPDATE 월별계좌상태 SET 상태구분코드 = '07'
+ WHERE 상태구분코드 <> '01'
+   AND 기준년월 = :BASE_DT
+   AND (계좌번호, 계좌일련번호) IN 
+   		(SELECT 계좌번호, 계좌일련번호
+   		   FROM 계좌원장
+   		  WHERE 개설일자 LIKE :STD_YM || '%');
+
+-- 튜닝
+/** [인덱스 구성]
+ * 1. 일별지수업종별거래_PK : [지수구분코드 + 지수업종코드 + 거래일자]
+ * 2. 일별지수업종별거래_X1 : [거래일자]
+ * */ 
+SELECT 거래일자
+	 , SUM(DECODE(지수구분코드, '1', 지수종가, 0)) KOSPI200_IDX	
+	 , SUM(DECODE(지수구분코드, '1', 누저거래량, 0)) KOSPI200_IDX_TRDVOL
+	 , SUM(DECODE(지수구분코드, '2', 지수종가, 0)) KOSPI200_IDX
+	 , SUM(DECODE(지수구분코드, '2', 누적거래량, 0)) KOSPI200_IDX_TRDVOL
+  FROM 일별지수업종별거래 A
+ WHERE 거래일자 BETWEEN :STARTDT AND :ENDDT
+   AND (지수구분코드, 지수업종코드) IN (('100' ,'1'), ('200', '3'))
+ GROUP BY 거래일자;
+ 
---
+++ sql-study/더좋은쿼리작성/README.md
@@ -0,0 +1,49 @@
+# 더 좋은 쿼리 작성
+
+1. IN 조건 vs. BETWEEN
+   - 의미와 용도에 맞게 선택
+   - IN
+     - Equal(=) 조건과 OR 연산(합집합) 결합
+     - Random Access(IO) 동작
+     - 검색 대상 값이 많을수록 인덱스 사용기회 감소
+     - 비 연속 값 검색
+     - 검색 대상이 적은 경우 적합
+   - BETWEEN
+     - Non-Equal(>= <=) 조건과 AND 연산 결합
+     - Sequence Access 동작
+     - 연속 값 검색 시 적합    
+
+2. NULL 고려한 집계 연산
+    - 대량 NULL 값을 가진 열의 경우
+      - 불필요한 NULL 데이터를 사전 필터링 한다.
+        - SELECT SUM(TRX_AMT) FROM dbo.MonyTrx
+        - WHERE TRX_AMT IS NOT NULL
+
+3. OLTP 쿼리의 기본 - Nested Loops Join 성능 이해
+    - 외부입력 : 검색 행 수가 더 적은 테이블
+    - 내부입력 : 반드시 Index 사용(없으면 생성) (SARG 준수)
+    - ON 절에서도 인덱스를 가공하지 말 것
+    - 불필요한 OUTER JOIN을 사용하지 말 것 (INNER JOIN으로 해결하는 방법 먼저 생각)
+    - 유지보수를 위한 권장 [WHERE 절 조건식 순서] 
+      - 같은 테이블 별칭끼리 묶어서
+      - 검색 주인공은 선두에
+        - WHERE t1.col1 = ?
+        - AND t1.col2 = ?
+        - AND t2.col1 = ?
+        - AND t2.col2 = ?
+        - AND t3.col1 
+
+4. Subquery 이해
+    - Join으로 변환 후, Join으로써 처리
+
+5. 성능 좋은 고급 쿼리 적용 예
+    - 중복 I/O 제거 = "같은 데이터는 2번 이상 주복해서 읽지 않는다"
+      - A. JOIN으로 변경
+      - B. 기준 결과 집합 선 처리 후 결합
+      - C. 행 복제
+    - 연산 순서 조정 - 더 나은 순서로 연산처리
+      - Ex. 결합(Join, Subquery) 전 Group 먼저
+      
+6. Group by 
+   - 예를 들어 두개의 컬럼을 가지고 Group by한다면 중복이 제거된다.
+   
