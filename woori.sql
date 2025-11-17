68c74d608cef8d8c046aec3ff8857a48eb3ca172
kaltz <96904103+keunoh@users.noreply.github.com>
2025-11-17 16:39:09 +0900

    Update woori.sql

--- woori.sql
+++ woori.sql
@@ -27,3 +27,55 @@ SELECT SUM(DECODE(SWMP_DCME_APV_TRN_DSCD, '1', 1, 0)) AS APV_CNT -- 승인건수
   FROM MY_TABLE
  WHERE 1 = 1
    AND MTMS_DT BETWEEN '20251001' AND '20251031'
+
+-- CASE Query -> 재미있는 쿼리인데 구분값을 하드코딩해주고
+-- 프로그램소스에서 그에따라 분기처리해서 SUM 값을 하나만 가져오는 방식
+
+SELECT 'CTUP' AS TRN_DIS
+		 ,  SUM(SUBSTRB(TLM_ALL_TXT, 88, 18)) AS AM_SUM
+  FROM MY_TABLE
+ WHERE cond1 = :input
+   AND 구분코드 = '21'
+   AND 구분코드 = :input2
+ GROUP BY 1
+ UNION ALL
+SELECT 'CTUPCNT'
+     , COUNT(*)
+  FROM MY_TABLE
+ WHERE cond1 = :input 
+   AND 구분코드 = '21'
+   AND 구분코드 = :input2
+ GROUP BY 1
+ UNION ALL
+SELECT 'PSNC'
+     , SUM(SUBSTRB(TLM_ALL_TXT, 1, 19))
+  FROM MY_TABLE
+ WHERE cond1 = :input
+   AND 구분코드 = '14'
+   AND 구분코드 = :input2
+ GROUP BY 1
+..... 쭉쭉
+
+-- 프로그램에서는 만약 구분코드값이 'CTUPCNT' 면
+-- 해당 컬럼에 값을 SET
+-- 그리고 input2값이 들어오기 때문에 해당건만 SQL에서 나올거임
+-- 즉 최대 row 2개(건수, 총금액)
+-- 그래서 for문을 하더라도 사실상 최대 2건인 셈
+for i {
+	if(Out.C_TrnDis[ll_Tnum] == 'CTUP') {
+			LNT2NUM(BIZOT->n_AmSum1, Out.l_AmSum[ll_Tnum]);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
